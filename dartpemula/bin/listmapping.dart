4c935e04966c750cd588e2e78362d4ffc4f41db9
Puteri Yudani <puteriyudani@gmail.com>
2021-03-16 20:16:38 +0700

    contain list

--- dartpemula/bin/listmapping.dart
+++ dartpemula/bin/listmapping.dart
@@ -33,21 +33,39 @@
 // }
 
 // fungsi dasar di dalam list
+// import 'dart:io';
+
+// main(List<String> arguments) {
+//   List<int> myList = [1, 2, 3, 4, 5, 6, 7];
+//   List<int> list = [1, 2, 3];
+
+//   myList.add(10);
+//   myList.addAll(list);
+//   myList.insert(1, 20);
+//   myList.insertAll(3, [30, 40, 20]);
+//   myList.remove(20);
+//   myList.removeLast();
+//   myList.removeAt(0);
+//   myList.removeRange(1, 4);
+//   myList.removeWhere((number) => number % 2 != 0);
+
+//   myList.forEach((bilangan) {
+//     print(bilangan);
+//   });
+// }
+
+// contain list
 import 'dart:io';
 
 main(List<String> arguments) {
   List<int> myList = [1, 2, 3, 4, 5, 6, 7];
   List<int> list = [1, 2, 3];
 
-  myList.add(10);
-  myList.addAll(list);
-  myList.insert(1, 20);
-  myList.insertAll(3, [30, 40, 20]);
-  myList.remove(20);
-  myList.removeLast();
-  myList.removeAt(0);
-  myList.removeRange(1, 4);
   myList.removeWhere((number) => number % 2 != 0);
+
+  if (myList.contains(6)) {
+    print("betul!");
+  }
 
   myList.forEach((bilangan) {
     print(bilangan);
