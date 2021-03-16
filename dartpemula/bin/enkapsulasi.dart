03e291af86366f8bc6af849fa992407fae117b45
Puteri Yudani <puteriyudani@gmail.com>
2021-03-16 21:31:34 +0700

    enkapsulasi (method dan properti)

---
+++ dartpemula/bin/enkapsulasi.dart
@@ -0,0 +1,21 @@
+// enkapsulasi (method dan properti)
+import 'dart:io';
+import 'package:dartpemula/persegi_panjang.dart';
+
+main(List<String> arguments) {
+  PersegiPanjang kotak1, kotak2;
+  double luasKotak1;
+
+  kotak1 = new PersegiPanjang();
+  kotak1.setPanjang(2);
+  kotak1.lebar = -3;
+
+  kotak2 = PersegiPanjang();
+  kotak2.setPanjang(double.tryParse(stdin.readLineSync()));
+  kotak2.lebar = double.tryParse(stdin.readLineSync());
+
+  luasKotak1 = kotak1.hitungLuas();
+
+  print(luasKotak1 + kotak2.hitungLuas());
+  print(kotak1.lebar);
+}
---
+++ dartpemula/lib/persegi_panjang.dart
@@ -0,0 +1,30 @@
+class PersegiPanjang {
+  double _panjang;
+  double _lebar;
+
+  void set lebar(double value) {
+    if (value < 0) {
+      value *= -1;
+    }
+    _lebar = value;
+  }
+
+  void setPanjang(double value) {
+    if (value < 0) {
+      value *= -1;
+    }
+    _panjang = value;
+  }
+
+  double get lebar {
+    return _lebar;
+  }
+
+  double getPanjang() {
+    return _panjang;
+  }
+
+  double hitungLuas() {
+    return this._panjang * _lebar;
+  }
+}
