39cff06f04f0db7f001f1876dc23c25bc897cdb9
Puteri Yudani <puteriyudani@gmail.com>
2021-03-18 09:28:45 +0700

    keyword as & is

--- dartpemula/bin/inheritance.dart
+++ dartpemula/bin/inheritance.dart
@@ -2,19 +2,33 @@ import 'dart:io';
 
 import 'package:dartpemula/hero.dart';
 import 'package:dartpemula/monster.dart';
+import 'package:dartpemula/monster_kecoa.dart';
 import 'package:dartpemula/monster_ubur_ubur.dart';
 
 main(List<String> arguments) async {
   Hero h = Hero();
   Monster m = Monster();
-  MonsterUburUbur u = MonsterUburUbur();
+  MonsterUburUbur monster = MonsterUburUbur();
+  List<Monster> monsters = [];
 
-  h.healthPoint = -10;
-  u.healthPoint = 3;
+  monsters.add(MonsterUburUbur());
+  monsters.add(MonsterKecoa());
+  monsters.add(MonsterUburUbur());
 
-  print("hero HP : " + h.healthPoint.toString());
-  print("monster HP : " + u.healthPoint.toString());
-  print(h.killAMonster());
-  print(u.eatHuman());
-  print(u.swim());
+  // h.healthPoint = -10;
+  // u.healthPoint = 3;
+
+  // print("hero HP : " + h.healthPoint.toString());
+  // print("monster HP : " + u.healthPoint.toString());
+  // print(h.killAMonster());
+  // print(u.eatHuman());
+  // print(u.swim());
+
+  print((monster as MonsterUburUbur).swim());
+
+  for (Monster m in monsters) {
+    if (m is MonsterUburUbur) {
+      print(m.swim());
+    }
+  }
 }
