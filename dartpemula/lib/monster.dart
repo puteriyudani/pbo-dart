6c7c661197f717871da39346dd33310db958f5fd
Puteri Yudani <puteriyudani@gmail.com>
2021-03-19 12:15:10 +0700

    menambah mixin pada monster

--- dartpemula/lib/monster.dart
+++ dartpemula/lib/monster.dart
@@ -1,8 +1,9 @@
 import 'package:dartpemula/character.dart';
 
 import 'package:dartpemula/character.dart';
+import 'package:dartpemula/drink_ability_mixin.dart';
 
-abstract class Monster extends Character {
+abstract class Monster extends Character with DrinkAbilityMixin {
   String eatHuman() => "Grr.. Delicious.. Yummy..";
   String move();
 }
--- dartpemula/lib/monster_ucoa.dart
+++ dartpemula/lib/monster_ucoa.dart
@@ -3,8 +3,9 @@ import 'package:dartpemula/flying_monster.dart';
 import 'package:dartpemula/monster_ubur_ubur.dart';
 
 class MonsterUcoa extends MonsterUburUbur
-    with DrinkAbilityMixin
-    implements FlyingMonster {
+    // with DrinkAbilityMixin
+    implements
+        FlyingMonster {
   @override
   String fly() {
     return "Terbang-terbang melayang";
